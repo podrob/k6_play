cb27a725abe0f9e49bd108d23d9bded4eace98be
podrob <rob.mcbryde@pod-point.com>
2022-09-27 16:43:34 +0100

    initial commit of k6 experiment

---
+++ script.js
@@ -0,0 +1,20 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+export const options = {
+    stages: [
+        { duration: '5s', target: 20 },
+        { duration: '10s', target: 10 },
+        { duration: '5s', target: 0 },
+      ],
+      thresholds: {
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+      }
+}
+
+export default function () {
+    const res = http.get('https://test.k6.io');
+    check(res, { 'status was 200': (r) => r.status == 200 })
+    sleep(1);
+}
