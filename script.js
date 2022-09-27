19c70507cbfab02e699732ac92c66a6e99c26a90
podrob <rob.mcbryde@pod-point.com>
2022-09-27 17:26:26 +0100

    easier thresholds to hit

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import { sleep } from 'k6'
 
 export const options = {
     stages: [
-        { duration: '5s', target: 20 },
-        { duration: '10s', target: 10 },
-        { duration: '5s', target: 0 },
+        { duration: '2s', target: 10 },
+        { duration: '5s', target: 5 },
+        { duration: '2s', target: 0 },
       ],
       thresholds: {
-        http_req_failed: ['rate<0.05'], // http errors should be less than 1%
-        http_req_duration: ['p(95)<2000'], // 95 percent of response times must be below 500ms
+        http_req_failed: ['rate<0.05'], // http errors should be less than 5%
+        http_req_duration: ['p(95)<2000'], // 95 percent of response times must be below 2seconds
       }
 }
 
